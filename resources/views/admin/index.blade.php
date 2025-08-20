d1a1323b4e04f0990a6d6b31fafdec3fe9e326fc
nooraalam1 <noorsomrat2000@gmail.com>
2025-08-20 14:03:13 +0600

    Multiauth Added

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Admin Dashboard</title>
+</head>
+<body>
+
+	<h1>Admin</h1>
+	<form method="POST" action="{{ route('logout') }}">
+	    @csrf
+
+	    <input type="submit" value="logout">
+	</form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +20,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin/dashboard',[HomeController::class,'index'])->middleware(['auth','admin']);
